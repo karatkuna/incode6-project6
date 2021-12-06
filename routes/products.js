99e189b0f3a9caed485e312df8472bdda3b04f92
karatkuna <karat.kuna@gmail.com>
2021-12-07 03:25:53 +1100

    create purchases route and template

--- routes/products.js
+++ routes/products.js
@@ -3,12 +3,13 @@ const db = require("../database")
 const router = express.Router()
 
 router.get("/", (req, res) => {
-  db.any("SELECT * FROM sales")
+  db.any("SELECT p.*, c.name AS categoryname FROM products p JOIN categories c ON c.id=p.category_id ORDER BY p.id DESC")
 
-  .then((sales) => {
-    res.render("pages/sales", {
-      pageTitle: 'Sales',
-      sales
+  .then((products) => {
+    console.log(products)
+    res.render("pages/products", {
+      pageTitle: 'Products',
+      products
     })
   })
 
@@ -19,13 +20,13 @@ router.get("/", (req, res) => {
 })
 
 router.get("/new", (req, res) => {
-  db.any("SELECT * FROM pricelist")
+  db.any("SELECT * FROM categories")
 
-  .then((pricelist) => {
-    console.log(pricelist)
-    res.render("pages/new-sales", {
-      pageTitle: 'New Sales',
-      pricelist
+  .then((categories) => {
+    console.log(categories)
+    res.render("pages/new-product", {
+      pageTitle: 'New Product',
+      categories
     })
   })
 
